f0a8feab1845a2023ceaa1772287cc48060a4edc
HakkinDavid <desr11@hotmail.com>
2025-02-17 19:29:43 -0800

    redo frontend

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,7 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter()
 	}
