95623008e81b644e0363f927c0836b312f872c96
HakkinDavid <desr11@hotmail.com>
2025-05-16 19:01:52 -0700

    Update Footer.svelte

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,4 +1,7 @@
-<footer class="bg-gray-800 text-gray-300 py-6 mt-auto">
+<script>
+    import { fly } from "svelte/transition";
+</script>
+<footer class="bg-gray-800 text-gray-300 py-6 mt-auto" in:fly={{'y': '200', 'duration': 1000, 'delay': 500}}>
     <div class="container mx-auto px-4">
         <div class="flex flex-col md:flex-row justify-between items-center">
             <p>&copy; 2025 David Santana, Hugo Manzano, Oscar Angulo</p>
